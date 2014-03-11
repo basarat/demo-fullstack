3b9ca79798b489d1391e6addedfe2fc1540ca73f
basarat <basaratali@gmail.com>
2014-03-12 01:17:18 +1100

    move away from jade to ejs

--- fullstack/server/server.js
+++ fullstack/server/server.js
@@ -13,7 +13,7 @@ var users = require('./routes/user');
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(favicon());
 app.use(logger('dev'));
